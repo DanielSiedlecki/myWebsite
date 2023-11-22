e5ce1a65a24d540c8c54601809f9650a4c96fd48
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-22 13:51:30 +0100

    style views

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,43 @@
-<template>sda</template>
+<template>
+  <div class="">
+    <div class="left-navbar">
+      <left-navbar />
+    </div>
 
-<script></script>
+    <div class="content">
+      <about-me></about-me>
+    </div>
+  </div>
+</template>
+
+<script>
+import AboutMe from "./components/AboutMe.vue";
+import LeftNavbar from "./components/LeftNavbar.vue";
+export default {
+  components: {
+    LeftNavbar,
+    AboutMe,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.left-navbar {
+  max-width: 250px;
+  width: 100%;
+}
+
+.content {
+  z-index: 2000;
+  height: 100vh;
+  width: auto;
+  margin-left: 250px;
+  background-color: #111111;
+}
+
+@media screen and (max-width: 1024px) {
+  .content {
+    margin-left: 54px;
+  }
+}
+</style>
