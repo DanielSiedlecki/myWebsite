1c17422ccaa54c54220a59b8996984fedb89a927
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-22 23:40:59 +0100

    style myskills

---
+++ src/components/MySkills.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="flex flex-col lg:flex-row bg-black h-full w-full">
+    <div
+      class="basis-full lg:basis-1/2 flex flex-col justify-center mb-10 ml-2"
+    >
+      <div class="title text-white lg:ml-10 mb-5">
+        <h1 class="text-2xl font-semibold mt-10 ml-1">Programming Skills</h1>
+      </div>
+      <div
+        class="flex flex-col items-center m-2"
+        v-for="item in skillsList"
+        :key="item"
+      >
+        <span
+          class="flex flex-row text-white font justify-between italic w-full lg:w-10/12"
+          ><p class="text-lg tracking-wide">{{ item.name }}</p>
+          <p class="text-lg">{{ item.percent }}</p></span
+        >
+        <div id="progress-bar-container" class="w-full lg:w-10/12 bg-gray-700">
+          <div
+            id="progress-bar"
+            class="bg-gray-400"
+            :style="{ width: item.percent }"
+          ></div>
+        </div>
+      </div>
+    </div>
+    <div class="basis-full lg:basis-1/2 ml-2">
+      <div class="title text-white mb-5">
+        <h1 class="text-2xl font-semibold ml-1 lg:mt-10">Other</h1>
+      </div>
+      <ul
+        class="list-image-checkmark m-2 ml-6"
+        v-for="item in skillsList"
+        :key="item"
+      >
+        <li>
+          <p class="text-xl text-white tracking-wider">{{ item.name }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+  data() {
+    return {
+      skillsList: [
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+        { name: "Skills", percent: "30%" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#progress-bar-container {
+  height: 3px;
+  border-radius: 50px;
+}
+
+#progress-bar {
+  height: 100%;
+  transition: width 0.3s;
+  border-radius: 50px;
+}
+</style>
