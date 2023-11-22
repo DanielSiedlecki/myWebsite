4ee1faa3465fa2af60313877d4101dc95fcf64f9
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-22 17:38:48 +0100

    style Title

---
+++ src/elements/sectionTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="flex items-center justify-center w-24 h-12"
+    style="background-color: #333333"
+  >
+    <p class="text-sm text-black uppercase font-medium" style="color: #a0a0a0">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style lang="scss" scoped></style>
