512b9ed6d2f1e90cb574181ca4a49d8d562e835d
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-22 23:40:50 +0100

    style button

--- src/elements/buttonElement.vue
+++ src/elements/buttonElement.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="button-container">
-    <button placeholder="Download ME"></button>
+  <div
+    class="button-container h-12 w-36 bg-white flex justify-center hover:w-40 animation"
+  >
+    <button type="button" class="text-sm tracking-wide">
+      <slot></slot>
+    </button>
   </div>
 </template>
 
@@ -9,3 +13,8 @@ export default {
   setup() {},
 };
 </script>
+<style scoped>
+.animation {
+  transition: 0.3s ease-in;
+}
+</style>
