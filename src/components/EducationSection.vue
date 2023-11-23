6c124f843a7746951a9e228166c208ad486dd635
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-23 13:45:42 +0100

    created educationsection

---
+++ src/components/EducationSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex flex-col lg:flex-row bg-black h-full w-full pt-5 pb-5 border"
+  ></div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
