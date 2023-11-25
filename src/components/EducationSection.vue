77a6bb21c7cdb7fc18a2217babff1122cbb0db71
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-25 03:19:39 +0100

    style Edu

--- src/components/EducationSection.vue
+++ src/components/EducationSection.vue
@@ -1,7 +1,79 @@
 <template>
-  <div
-    class="flex flex-col lg:flex-row bg-black h-full w-full pt-5 pb-5 border"
-  ></div>
+  <div class="mt-10 mb-5">
+    <div class="flex flex-col lg:flex-row gap-5 bg-black w-full pb-5">
+      <div class="basis-full lg:basis-1/2 flex flex-col pl-3 lg:pl-0">
+        <div class="title lg:ml-10 mt-5">
+          <h1 class="text-white text-2xl font-semibold mb-5 mt-10">
+            Work Experience
+          </h1>
+        </div>
+        <ol class="relative border-s border-gray-200 lg:self-center ml-5">
+          <li class="mb-5 ms-4">
+            <div
+              class="absolute w-4 h-4 bg-gray-200 rounded-full mt-1.5 -start-2 border border-white"
+            ></div>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-300"
+              >August 2023 - ongoing</time
+            >
+            <h3 class="text-lg font-semibold text-gray-300">Karpato</h3>
+            <h4 class="text-sm font-medium text-gray-300 max-w-sm">
+              Junior Salesforce Developer
+            </h4>
+            <p class="mb-4 text-sm font-normal text-gray-500 max-w-sm">
+              Get access to over 20+ pages including a dashboard layout, charts,
+              kanban board, calendar, and pre-order E-commerce & Marketing
+              pages.
+            </p>
+          </li>
+          <li class="mb-5 ms-4">
+            <div
+              class="absolute w-4 h-4 bg-gray-200 rounded-full mt-1.5 -start-2 border border-white"
+            ></div>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400"
+              >February 2023 - July 2023</time
+            >
+            <h3 class="text-lg font-semibold text-gray-300">ZETO - ŚWIDNICA</h3>
+            <h4 class="text-sm font-medium text-gray-300 max-w-sm">
+              Frontend Developer
+            </h4>
+            <p class="text-sm font-normal text-gray-500 max-w-sm">
+              All of the pages and components are first designed in Figma and we
+              keep a parity between the two versions even as we update the
+              project.
+            </p>
+          </li>
+        </ol>
+      </div>
+      <div class="basis-full lg:basis-1/2 flex flex-col pl-3 lg:pl-0">
+        <div class="title mt-5">
+          <h1 class="text-white text-2xl font-semibold mb-5 mt-10">
+            Education
+          </h1>
+        </div>
+        <ol class="relative border-s border-gray-200 ml-5">
+          <li class="mb-5 ms-4">
+            <div
+              class="absolute w-4 h-4 bg-gray-200 rounded-full mt-1.5 -start-2 border border-white"
+            ></div>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400"
+              >October 2020 - ongoing</time
+            >
+            <h3 class="text-lg font-semibold text-gray-300 max-w-sm">
+              WSB Merito University
+            </h3>
+            <h4 class="text-sm font-medium text-gray-300 max-w-sm">
+              Network Computer Engerring
+            </h4>
+            <p class="mb-4 text-sm font-normal text-gray-500 max-w-sm">
+              Get access to over 20+ pages including a dashboard layout, charts,
+              kanban board, calendar, and pre-order E-commerce & Marketing
+              pages.
+            </p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
