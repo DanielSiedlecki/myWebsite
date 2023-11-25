a54632dc82fe83eed1d52815a94b23ad7a055b47
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-25 03:19:26 +0100

    style certifications Section

--- src/components/CertificationsCourse.vue
+++ src/components/CertificationsCourse.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class=""></div>
+  <div class="container pl-2 lg:pl-0">
+    <div class="title mt-5 mb-10">
+      <h1 class="text-white text-2xl font-semibold mt-10">Certificate</h1>
+    </div>
+    <div class="flex flex-col md:flex-row items-center gap-5 mt-5 mb-5">
+      <div class="flex basis-full md:basis-1/2 lg:basis-1/3 w-4/6">
+        <span class="flex flex-col items-center gap-2">
+          <i class="bi bi-patch-check-fill text-white text-4xl"></i>
+          <p class="text-white text-xl w-5/6 text-center">
+            Get access to over 20+ pages including a dashboard
+          </p>
+        </span>
+      </div>
+      <div class="flex flex-row md:basis-1/2 lg:basis-1/3 w-4/6">
+        <span class="flex flex-col items-center gap-2">
+          <i class="bi bi-patch-check-fill text-white text-4xl"></i>
+          <p class="text-white text-xl w-5/6 text-center">
+            Get access to over 20+ pages including a dashboard
+          </p>
+        </span>
+      </div>
+      <div class="flex flex-row md:basis-1/2 lg:basis-1/3 w-4/6">
+        <span class="flex flex-col items-center gap-2">
+          <i class="bi bi-patch-check-fill text-white text-4xl"></i>
+          <p class="text-white text-xl w-5/6 text-center">
+            Get access to over 20+ pages including a dashboard
+          </p>
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style></style>
->
