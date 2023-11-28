6a28d71c8c3eb776c94ef6fb0fd5cc79d38bb0b6
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-28 01:22:47 +0100

    created recaptcha

---
+++ src/elements/recaptchaElement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container w-96 flex justify-center mt-2">
+    <form>
+      <div
+        class="g-recaptcha"
+        ref="recaptchaElement"
+        data-sitekey="6LdUFOMmAAAAAB9zknMxDPOwg2gBR2ek7yz1Ya14"
+        data-callback="onRecaptchaVerify"
+        data-hl="en"
+        data-theme="dark"
+      ></div>
+      <br />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
