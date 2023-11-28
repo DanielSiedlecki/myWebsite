d1897ae7b92a201f913bb4dbf3ceaa4cc65165f7
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-28 02:36:18 +0100

    remove container

--- src/elements/recaptchaElement.vue
+++ src/elements/recaptchaElement.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="container w-96 flex justify-center mt-2">
-    <form>
-      <div
-        class="g-recaptcha"
-        ref="recaptchaElement"
-        data-sitekey="6LdUFOMmAAAAAB9zknMxDPOwg2gBR2ek7yz1Ya14"
-        data-callback="onRecaptchaVerify"
-        data-hl="en"
-        data-theme="dark"
-      ></div>
-      <br />
-    </form>
-  </div>
+  <form>
+    <div
+      class="g-recaptcha"
+      ref="recaptchaElement"
+      data-sitekey="6LdUFOMmAAAAAB9zknMxDPOwg2gBR2ek7yz1Ya14"
+      data-callback="onRecaptchaVerify"
+      data-hl="en"
+      data-theme="dark"
+    ></div>
+    <br />
+  </form>
 </template>
 
 <script>
